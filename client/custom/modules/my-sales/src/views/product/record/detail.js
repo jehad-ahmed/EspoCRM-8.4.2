c5a6c052dfcc9833b39a9d78d53f6223ea76052f
jehad ahmad <eeng.jehad.ahmad@gmail.com>
2025-01-06 13:35:06 +0300

    for save

--- client/custom/modules/my-sales/src/views/product/record/detail.js
+++ client/custom/modules/my-sales/src/views/product/record/detail.js
@@ -12,10 +12,10 @@ define('my-sales:views/product/record/detail', ['views/record/detail'], function
             if(this.model.get('requiredQuantity') && this.model.get('salePrice')){
                 this.model.set('totalPrice', this.model.get('requiredQuantity') * this.model.get('salePrice'));
                 this.model.set('totalQuantity', this.model.get('totalQuantity') - this.model.get('requiredQuantity'));
-                this.model.save({totalQuantity: this.model.get('totalQuantity')},{patch: true});
-                this.model.save({totalPrice: this.model.get('totalPrice')},{patch: true});
-            }
 
+            }
+            this.model.save({totalQuantity: this.model.get('totalQuantity')},{patch: true});
+            this.model.save({totalPrice: this.model.get('totalPrice')},{patch: true});
         }
     });
 });
